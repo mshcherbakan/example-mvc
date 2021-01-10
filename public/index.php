49488d2196d79b9f666e4682fc7b407cc6319aa5
Maksym Shcherbakan <m.shcherbakan@gmail.com>
2021-01-10 17:45:41 +0200

    remove comments

--- public/index.php
+++ public/index.php
@@ -8,8 +8,6 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// add autoload
-// new place for conflict
 require 'autoload.php';
 
 $autoloader = new Psr4AutoloaderClass();
